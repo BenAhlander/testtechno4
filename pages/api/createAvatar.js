fcaf6e65394b5cc5ef1945dbf4706fa739dd47fd
Ben <ben.ahlander@gmail.com>
2023-08-11 17:42:21 -0600

    openai should not block

--- pages/api/createAvatar.js
+++ pages/api/createAvatar.js
@@ -15,18 +15,24 @@ export default async function handler(req, res) {
       break;
     case "POST":
       try {
+        let message;
         const { name, image, interests } = JSON.parse(req.body);
-        const chat_completion = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [
-            {
-              role: "user",
-              content: `Write a 1 paragraph, 3 sentence, description about a person named ${name} who is interested in ${interests}`,
-            },
-          ],
-        });
-        const message = chat_completion.data.choices[0].message.content;
-        console.log("CHATGPT: ", message);
+        try {
+          const chat_completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "user",
+                content: `Write a 1 paragraph, 3 sentence, description about a person named ${name} who is interested in ${interests}`,
+              },
+            ],
+          });
+          message = chat_completion.data.choices[0].message.content;
+          console.log("CHATGPT: ", message);
+        } catch (error) {
+          console.error(error);
+          message = "No description available";
+        }
         await sql`INSERT INTO avatars (name, image, interests, description) VALUES (${name}, ${image}, ${interests}, ${message});`;
       } catch (error) {
         console.error(error);
